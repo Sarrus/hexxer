56c610454b46565c24328dd8313c1636c1939d45
Michael Bell <michael@tertheya.com>
2021-01-03 21:38:32 +0000

    Configured to find all valid solutions

--- main.c
+++ main.c
@@ -8,41 +8,34 @@ int main()
 {
     HEXAGON currentHexagon;
 
-//    for(int i = 0; i < TOTAL_SEGMENTS; i++)
-//    {
-//        unsigned long solution = 0;
-//        for(int j = 0; j < cellRelationships[i].countOfRelationships; j++)
-//        {
-//            solution |= (unsigned long)YELLOW << cellRelationships[i].relatedCells[j] * 2;
-//            longToHexagon(solution, &currentHexagon, false);
-//        }
-//        printf("%i:\r\n", i);
-//        printHexagon(&currentHexagon);
-//        getchar();
-//    }
+    //unsigned long lastReportAt = 0;
 
-    unsigned long lastReportAt = 0;
+    unsigned long solutionsFound = 0;
 
     for(unsigned long i = 0; i < TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED; i++)
     {
         if(validateSolution(i))
         {
-            printf("Solution found at hexagon no. %lu\r\n", i);
-            printf("%f%% of all hexagons tried\r\n", 100 * (float)i / (float)TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED);
-            longToHexagon(i, &currentHexagon, false);
-            printHexagon(&currentHexagon);
-            return EXIT_SUCCESS;
+            solutionsFound++;
+            printf("Solution found at hexagon no. %lu ", i);
+            printf("%f%% of all hexagons tried, ", 100 * (float)i / (float)TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED);
+            printf("%lu solutions found so far.\r\n", solutionsFound);
+            //longToHexagon(i, &currentHexagon, false);
+            //printHexagon(&currentHexagon);
+            //return EXIT_SUCCESS;
         }
 
-        if((clock() > CLOCKS_PER_SEC) && (lastReportAt < (clock() - CLOCKS_PER_SEC)))
-        {
-            printf("Current hexagon no. %lu\r\n", i);
-            printf("%f%% of all hexagons tried\r\n", 100 * (float)i / (float)TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED);
-            longToHexagon(i, &currentHexagon, false);
-            printHexagon(&currentHexagon);
-            lastReportAt = clock();
-        }
+//        if((clock() > CLOCKS_PER_SEC) && (lastReportAt < (clock() - CLOCKS_PER_SEC)))
+//        {
+//            //printf("Current hexagon no. %lu\r\n", i);
+//            //printf("%f%% of all hexagons tried\r\n", 100 * (float)i / (float)TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED);
+//            longToHexagon(i, &currentHexagon, false);
+//            printHexagon(&currentHexagon);
+//            lastReportAt = clock();
+//        }
     }
 
-    return EXIT_FAILURE;
+    printf("Tried all possible hexagons.\r\n");
+
+    return EXIT_SUCCESS;
 }
