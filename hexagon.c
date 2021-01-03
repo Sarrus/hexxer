29d32035616879b46e07daeacce75fe5ecb6d3e4
Michael Bell <michael@tertheya.com>
2021-01-03 20:26:51 +0000

    Added check for the count of reds around a green

--- hexagon.c
+++ hexagon.c
@@ -7,6 +7,33 @@
 #include <stdbool.h>
 #include "hexagon.h"
 
+const struct {
+    char countOfRelationships;
+    char relatedCells[6];
+} cellRelationships[TOTAL_SEGMENTS] = {
+        {3, {1, 3, 4}}, // 0
+        {4, {0, 2, 4, 5}}, // 1
+        {3, {1, 5, 6}}, // 2
+        {4, {0, 4, 7, 8}}, // 3
+        {6, {0, 1, 3,5,8,9}}, // 4
+        {6, {1, 2, 4, 6, 9, 10}}, // 5
+        {4, {2, 5, 10, 11}}, // 6
+        {3, {3, 8, 12}}, // 7
+        {6, {3, 4, 7, 9, 12, 13}}, // 8
+        {6, {4, 5, 8, 10, 13, 14}}, // 9
+        {6, {5, 6, 9, 11, 14, 15}}, // 10
+        {3, {6, 10, 15}}, // 11
+        {4, {7, 8, 13, 16}}, // 12
+        {6, {8, 9, 12, 14, 16, 17}}, // 13
+        {6, {9, 10, 13, 15, 17, 18}}, // 14
+        {4, {10, 11, 14, 18}}, // 15
+        {3, {12, 13, 17}}, // 16
+        {4, {13, 14, 16, 18}}, // 17
+        {3, {14, 15, 17}}
+};
+
+
+
 char colourToChar(COLOUR colour)
 {
     switch(colour)
@@ -128,28 +155,37 @@ void longToHexagon(unsigned long number, HEXAGON * hexagon, bool lockLeftRed)
 
 bool validateSolution(unsigned long solution)
 {
-    unsigned int reds = 0;
-    unsigned int yellows = 0;
-    unsigned int greens = 0;
-    unsigned int blues = 0;
+    char reds = 0;
+    char yellows = 0;
+    char greens = 0;
+    char blues = 0;
 
-    for(unsigned int i = 0; i < TOTAL_SEGMENTS_WITH_LEFT_RED_LOCKED; i++)
+    char redsAt[TOTAL_SEGMENTS];
+    char yellowsAt[TOTAL_SEGMENTS];
+    char greensAt[TOTAL_SEGMENTS];
+    char bluesAt[TOTAL_SEGMENTS];
+
+    for(char i = 0; i < TOTAL_SEGMENTS_WITH_LEFT_RED_LOCKED; i++)
     {
         switch((solution >> i * 2) % 4)
         {
             case RED:
+                redsAt[reds] = i;
                 reds++;
                 break;
 
             case YELLOW:
+                yellowsAt[yellows] = i;
                 yellows++;
                 break;
 
             case GREEN:
+                greensAt[greens] = i;
                 greens++;
                 break;
 
             case BLUE:
+                bluesAt[blues] = i;
                 blues++;
                 break;
         }
@@ -162,7 +198,30 @@ bool validateSolution(unsigned long solution)
             || (blues < 3)
             )
     {
+        // Less than three of a colour found
         return false;
+    }
+
+    for(char i = 0; i < greens; i++)
+    {
+        char redsAroundGreen = 0;
+
+        for(char j = 0; j < cellRelationships[greensAt[i]].countOfRelationships; j++)
+        {
+            for(char k = 0; k < reds; k++)
+            {
+                if(redsAt[k] == cellRelationships[greensAt[i]].relatedCells[j])
+                {
+                    redsAroundGreen++;
+                }
+            }
+        }
+
+        if(redsAroundGreen != 3)
+        {
+            // green not surrounded by three reds found
+            return false;
+        }
     }
 
     return true;
