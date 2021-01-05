78cc0ced6383a8af7288a42b7c69ba692073d6b0
Michael Bell <michael@tertheya.com>
2021-01-05 21:46:20 +0000

    Added simple threading

--- main.c
+++ main.c
@@ -3,36 +3,24 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <time.h>
+#include <pthread.h>
 #include "hexagon.h"
 
-int main(int argc, char ** argv)
+#define MAX_PARALLEL_JOBS 256
+
+bool printHexagons = false;
+unsigned long parallelJobs = 0;
+
+struct solverThreadConfig
 {
-    opterr = true;
+    HEXAGON_AS_INT firstHexagon;
+    HEXAGON_AS_INT lastHexagon;
+};
 
-    int option;
+#define SOLVER_THREAD_CONFIG struct solverThreadConfig
 
-    bool printHexagons = false;
-
-    while((option = getopt(argc, argv, "hp")) != -1)
-    {
-        switch(option)
-        {
-            case 'h':
-                printf("Usage: hexxer [options]\r\n");
-                printf("  -h  Display this help.\r\n");
-                printf("  -p  Render and print all discovered solutions.\r\n");
-                return EXIT_SUCCESS;
-
-            case 'p':
-                printHexagons = true;
-                break;
-
-            case '?':
-            default:
-                return EXIT_FAILURE;
-        }
-    }
-
+void solveInSerial()
+{
     HEXAGON_AS_INT solutionsFound = 0;
     HEXAGON renderedHexagon;
 
@@ -53,6 +41,117 @@ int main(int argc, char ** argv)
     }
 
     printf("Tried all possible hexagons.\r\n");
+}
+
+void * solverThread(void * config)
+{
+    SOLVER_THREAD_CONFIG * solverConfig = config;
+    HEXAGON renderedHexagon;
+
+    for(HEXAGON_AS_INT i = solverConfig->firstHexagon; i < solverConfig->lastHexagon; i++)
+    {
+        if(validateSolution(i))
+        {
+            //solutionsFound++;
+            printf("Solution found at hexagon no. %lu \r\n", i);
+            //printf("%f%% of all hexagons tried, ", 100 * (float)i / (float)TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED);
+            //printf("%lu solutions found so far.\r\n", solutionsFound);
+            if(printHexagons)
+            {
+                longToHexagon(i, &renderedHexagon, false);
+                printHexagon(&renderedHexagon);
+            }
+        }
+    }
+
+    //printf("This solver's range is: %lu to %lu.\r\n", solverConfig->firstHexagon, solverConfig->lastHexagon);
+    return NULL;
+}
+
+void solveInParallel()
+{
+    pthread_t solverThreads[MAX_PARALLEL_JOBS];
+    SOLVER_THREAD_CONFIG threadConfigs[MAX_PARALLEL_JOBS];
+
+    HEXAGON_AS_INT hexagonsPerSolver = TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED / parallelJobs;
+    HEXAGON_AS_INT hexagonAllocation = 0;
+
+    for(unsigned long i = 0; i < parallelJobs; i++)
+    {
+        threadConfigs[i].firstHexagon = hexagonAllocation;
+        hexagonAllocation += hexagonsPerSolver;
+
+        if(!i)
+        {
+            hexagonAllocation += TOTAL_HEXAGONS_WITH_LEFT_RED_LOCKED % parallelJobs;
+        }
+
+        threadConfigs[i].lastHexagon = hexagonAllocation;
+
+        if(pthread_create(&solverThreads[i], NULL, solverThread, &threadConfigs[i]))
+        {
+            printf("Failed to create enough solver threads.\r\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for(unsigned long i = 0; i < parallelJobs; i++)
+    {
+        pthread_join(solverThreads[i], NULL);
+    }
+
+    printf("Tried all possible hexagons.\r\n");
+}
+
+int main(int argc, char ** argv)
+{
+    opterr = true;
+
+    int option;
+
+    while((option = getopt(argc, argv, "hj:p")) != -1)
+    {
+        switch(option)
+        {
+            case 'h':
+                printf("Usage: hexxer [options]\r\n");
+                printf("  -h  Display this help.\r\n");
+                printf("  -j  Number of parallel jobs to run. (Runs in serial mode if unspecified.)\r\n");
+                printf("  -p  Render and print all discovered solutions.\r\n");
+                return EXIT_SUCCESS;
+
+            case 'j':
+                parallelJobs = strtoul(optarg, NULL, 10);
+                if(!parallelJobs)
+                {
+                    printf("Invalid number of parallel jobs.\r\n");
+                    return EXIT_FAILURE;
+                }
+                else if(parallelJobs > MAX_PARALLEL_JOBS)
+                {
+                    printf("Parallel jobs limited to %i.\r\n", MAX_PARALLEL_JOBS);
+                    return EXIT_FAILURE;
+                }
+                break;
+
+            case 'p':
+                printHexagons = true;
+                break;
+
+            case '?':
+            default:
+                return EXIT_FAILURE;
+        }
+    }
+
+    if(parallelJobs)
+    {
+        solveInParallel();
+    }
+    else
+    {
+        solveInSerial();
+    }
 
     return EXIT_SUCCESS;
 }
